1fabc121eb894cde9cc057148b58a5d7cf4380e3
SigTermGh <tarantino.1760424@studenti.uniroma1.it>
2021-06-15 18:53:51 +0200

    first day

--- pca_test.py
+++ pca_test.py
@@ -1,12 +1,4 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.cluster import MiniBatchKMeans
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.metrics.cluster import (
-    completeness_score,
-    homogeneity_score,
-    adjusted_rand_score,
-)
 from sklearn.decomposition import TruncatedSVD
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Normalizer
@@ -14,17 +6,13 @@ from sklearn.preprocessing import Normalizer
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import pandas as pd
 
-import os
+from string import punctuation, digits
 
-
-from string import punctuation
-
-import random
 
 import numpy as np
 
-from time import time
 
 sw = set(stopwords.words("english"))
 sw.add("removed")
@@ -33,6 +21,12 @@ lemmatizer = WordNetLemmatizer()
 SVD_DIMENSIONS = 10
 VEC_MIN_DOCS = 3
 N_WORDS = 10
+
+
+def wordcloud_from_cvs(filename, nwords, size):
+    ds = pd.read_csv(filename)
+    ds = ds["Summary"].dropna().to_list()
+    return work(ds, nwords, size)
 
 
 def analyze_text(text):
@@ -44,6 +38,7 @@ def analyze_text(text):
         .decode("ascii")
         .strip()
         .translate(str.maketrans(punctuation, " " * len(punctuation)))
+        .translate(str.maketrans(digits, " " * len(digits)))
     )
     stem = []
     # lemmatize, remove stop words
@@ -54,45 +49,39 @@ def analyze_text(text):
     return stem
 
 
-def work(X):
-
-    # with open("comments_science") as corpus:
-    #     X = eval(corpus.read())
+def work(X, nwords, size):
 
     vectorizer = TfidfVectorizer(
         analyzer=analyze_text, min_df=VEC_MIN_DOCS, stop_words="english"
     )
 
-    s0 = time()
-    print("Fitting the vectorizer... ")
     X_train = vectorizer.fit_transform(X)
-
-    vec_shape = X_train.shape
-
-    print("Dataset in original space has shape %s" % str(vec_shape))
-
-    s0 = time()
     svd = TruncatedSVD(n_components=SVD_DIMENSIONS)
     normalizer = Normalizer(copy=False)
     pipeline = make_pipeline(svd, normalizer)
 
-    print("Fitting SVD/Normalizer pipeline... ")
     X_train = pipeline.fit_transform(X_train)
-    print("Done in %.3lfs" % (time() - s0))
-
-    dimred_shape = X_train.shape
 
     mean = np.mean(X_train, axis=0).reshape(1, -1)
-
-    print(mean)
 
     # print relevant terms for each cluster
     original_space_centroids = svd.inverse_transform(mean)
     order_centroids = original_space_centroids.argsort()[:, ::-1]
 
     terms = vectorizer.get_feature_names()
-    for ind in order_centroids[0, :N_WORDS]:
-        print(" %s" % terms[ind])
+
+    x = []
+    y = []
+
+    for idx in order_centroids[0, :nwords]:
+        x.append(terms[idx])
+        y.append(original_space_centroids[0][idx])
+    y = np.rint(np.interp(y, [min(y), max(y)], size)).tolist()
+
+    return list(zip(x, y))
+
+    # for ind in order_centroids[0, :N_WORDS]:
+    #     print(" %s" % terms[ind])
 
     # for i in range(N_CLUSTERS):
     #     print("Cluster %d:" % i)
@@ -114,9 +103,11 @@ def work(X):
 
 
 if __name__ == "__main__":
-    ds = []
-    f = open("dataset.csv", "r")
-    for line in f.readlines():
-        ds.append(line.split(",")[-1].strip())
-
-    work(ds)
+    # ds = []
+    # f = open("dataset.csv", "r")
+    # for line in f.readlines():
+    #     ds.append(line.split(",")[-1].strip())
+    ds = pd.read_csv("dataset.csv")
+    ds = ds["Summary"].dropna().to_list()
+    # exit()
+    work(ds, 10)
--- server.py
+++ server.py
@@ -1,9 +1,33 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask.templating import render_template
+from pca_test import wordcloud_from_cvs
+import random
 
 app = Flask(__name__, template_folder="templates")
 
 
-@app.route("/")
+@app.route("/world_map")
+def world_map():
+    return render_template("world_map.html")
+
+
+@app.route("/wc_data")
+def wc_data():
+
+    words = wordcloud_from_cvs("dataset.csv", 25, (25, 75))
+    return jsonify({"data": words})
+
+
+@ app.route("/word_cloud")
+def word_cloud():
+    # return Flask.send_static_file("/templates/world_map.html")
+    return render_template("word_cloud.html")
+
+
+@ app.route("/")
 def main():
     return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=31337, debug=True)
